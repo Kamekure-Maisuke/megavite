d0b5ef103b3e9ee56b400f5ec133b5f4dce7a383
KamekuraMaisuke <taisukeoono350@gmail.com>
2021-10-14 23:34:46 +0900

    tsv読み取り処理ボイラー

--- src/components/Nogi.vue
+++ src/components/Nogi.vue
@@ -1,3 +1,35 @@
+<script setup>
+import { ref } from 'vue'
+import * as d3 from 'd3'
+
+// 独立したリアクティブデータ
+const singles = ref(null)
+
+// 曲取得関数
+async function getSingles(){
+  try{
+    singles.value = await d3.tsv("../../data/single.tsv",(d) => {
+      return {
+        release: d['発売日'],
+        title: d['タイトル']
+      }
+    })
+  } catch(err){
+    console.error('🔥', err)
+  }
+}
+
+// 取得
+// ※toplevel awaitだとsuspenseタグが必要のため、保留。
+(async () => {
+  await getSingles()
+})()
+</script>
+
 <template>
-  <p>Nogi画面</p>
+  <ul>
+    <li v-for="single in singles" :key="single">
+      {{ single.release }}: {{ single.title }}
+    </li>
+  </ul>
 </template>
